b4f69d2a934cf72f322041f77d6fd592e252a992
Daniel Cardona <danielcardona288459@correo.itm.edu.co>
2025-02-28 12:56:01 -0500

    Desarrollo de la tarea para la casa

--- obj/Debug/net8.0/csharapigenerica.AssemblyInfo.cs
+++ obj/Debug/net8.0/csharapigenerica.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("csharapigenerica")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+262823906437140c26c06c6bb20ff1cb5e27b5de")]
 [assembly: System.Reflection.AssemblyProductAttribute("csharapigenerica")]
 [assembly: System.Reflection.AssemblyTitleAttribute("csharapigenerica")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
